f261f5e3bbede1fb1913f5c734918198c41cda4e
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2023-04-22 14:27:59 +0530

    removed api call made to tiny url on localhost

--- controllers/file/file.controller.js
+++ controllers/file/file.controller.js
@@ -19,116 +19,117 @@ const deleteUploads = require("../../utils/deleteUploads");
  * @description - This function is used to upload files.
  */
 module.exports.uploadFile = catchAsync(async (req, res, next) => {
-    
-    const { password, uploadPin } = req.body;
-    const fileData = req.file;
+	const { password, uploadPin } = req.body;
+	const fileData = req.file;
 
-    if(fileData.size > 1000000){
-        if(!uploadPin) {
-            req.flash("error", "File size is too large. Upload pin is required to upload files greater than 1MB.");
-            return res.redirect("/");
-        }
-        
-        if(uploadPin !== process.env.UPLOAD_PIN) {
-            req.flash("error", "Expired upload pin.");
-            return res.redirect("/");
-        }
-    }
+	if (fileData.size > 1000000) {
+		if (!uploadPin) {
+			req.flash(
+				"error",
+				"File size is too large. Upload pin is required to upload files greater than 1MB."
+			);
+			return res.redirect("/");
+		}
 
-    // Check if file is already exists.
-    const existingFile = await File.findOne({
-        originalname: fileData.originalname
-    });
+		if (uploadPin !== process.env.UPLOAD_PIN) {
+			req.flash("error", "Expired upload pin.");
+			return res.redirect("/");
+		}
+	}
 
-    if(existingFile){
-        const fileLink = existingFile.shortUrl;
+	// Check if file is already exists.
+	const existingFile = await File.findOne({
+		originalname: fileData.originalname,
+	});
 
-        return res.render('home', {
-            message: `File with name ${fileData.originalname} already exists at ⬇️`,
-            fileLink
-        });
-    };
-   
-    if(password != null && password != ""){
-        fileData.password = password;
-    }
+	if (existingFile) {
+		const fileLink = existingFile.shortUrl || existingFile.longUrl;
 
-    const file = new File(fileData);
-    
-    const fileLink = await getTinyUrl(
-        process.env.ACCESS_TOKEN, 
-        `${req.headers.origin}/file/${file._id}`
-    );
-    file.shortUrl = fileLink;
-    await file.save();
-        
-    return res.render("home", {
-        message: "Your file is uploaded to",
-        fileLink
-    });
+		return res.render("home", {
+			message: `File with name ${fileData.originalname} already exists at ⬇️`,
+			fileLink,
+		});
+	}
+
+	if (password != null && password != "") {
+		fileData.password = password;
+	}
+
+	const file = new File(fileData);
+
+	let fileLink = `${req.headers.origin}/file/${file._id}`;
+	if (process.env.NODE_ENV === "production") {
+		file.shortUrl = await getTinyUrl(
+			process.env.ACCESS_TOKEN,
+			`${req.headers.origin}/file/${file._id}`
+		);
+        fileLink = file.shortUrl;
+	} else {
+		file.longUrl = `${req.headers.origin}/file/${file._id}`;
+	}
+
+	await file.save();
+
+	return res.render("home", {
+		message: "Your file is uploaded to",
+		fileLink,
+	});
 });
-
 
 /**
  * @description -  This function is used to generate download link.
- * 
+ *
  */
-module.exports.genDownloadLink = catchAsync(async (req, res, _ ) => {
-    const {
-        file,
-        downloadPath,
-        originalname
-    } = req;
+module.exports.genDownloadLink = catchAsync(async (req, res, _) => {
+	const { file, downloadPath, originalname } = req;
 
-    if(file.password != null){
-        if(req.body.password == null){
-            req.flash("error", "Password is required to download this file.");
-            return res.render("password")
-        } else {
-            const match = await file.checkPassword(req.body.password);
-            if(!match) {
-                req.flash("error", "Password is incorrect.");
-                return res.render("password")
-            };
-            
-            file.downloadCount += 1;
-            await file.save();
+	if (file.password != null) {
+		if (req.body.password == null) {
+			req.flash("error", "Password is required to download this file.");
+			return res.render("password");
+		} else {
+			const match = await file.checkPassword(req.body.password);
+			if (!match) {
+				req.flash("error", "Password is incorrect.");
+				return res.render("password");
+			}
 
-            setTimeout(deleteUploads, 5000);
-            
-            return res.download(downloadPath, originalname);
-        }
-    } 
+			file.downloadCount += 1;
+			await file.save();
 
-    file.downloadCount += 1;
-    await file.save();
+			setTimeout(deleteUploads, 5000);
 
-    setTimeout(deleteUploads, 5000);
-    
-    return res.download(downloadPath, originalname);
+			return res.download(downloadPath, originalname);
+		}
+	}
+
+	file.downloadCount += 1;
+	await file.save();
+
+	setTimeout(deleteUploads, 5000);
+
+	return res.download(downloadPath, originalname);
 });
 
 /**
  * @description - This function is used to clear the uploads folder if it exists.
  */
-module.exports.clearUploads = (req, res)=>{
-    if(fs.existsSync('./uploads')){
-        fs.rm('./uploads', { recursive: true }, (err)=>{
-            if(err) {
-                return res.send({
-                    message: "Error while deleting uploads folder",
-                });
-            }
-            else {
-                return res.send({
-                    message: "Uploads folder deleted",
-                });
-            }
-        });
-        
-    } else {
-        return res.send({
-            message: "Uploads folder does not exists",
-        });
-    }
-}
+module.exports.clearUploads = (req, res) => {
+	if (fs.existsSync("./uploads")) {
+		fs.rm("./uploads", { recursive: true }, (err) => {
+			if (err) {
+				return res.send({
+					message: "Error while deleting uploads folder",
+				});
+			} else {
+				return res.send({
+					message: "Uploads folder deleted",
+				});
+			}
+		});
+	} else {
+		return res.send({
+			message: "Uploads folder does not exists",
+		});
+	}
+};
